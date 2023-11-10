20a80c52bc59a5f158dfb60d42524a25cca28370
douglas <douglasrc1912@gmail.com>
2023-11-10 20:15:19 -0300

    Atualizado

--- HelloWord/src/helloword/Helloword.java
+++ HelloWord/src/helloword/Helloword.java
@@ -1,10 +1,10 @@
 package helloword;
 
-public class Helloword {
+public class HelloWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Word");
-	}
 
+		System.out.println("Hello World!");
+	}
 }
